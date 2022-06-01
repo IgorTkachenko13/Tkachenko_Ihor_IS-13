7e520bf36e6a183d2ed6f745d8ac0ec39da61418
IgorTkachenko13 <106605598+IgorTkachenko13@users.noreply.github.com>
2022-06-01 18:43:22 +0300

    Add files via upload

---
+++ lab2/Exercise_2/Carrier.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Carrier
+    {
+        public string CarrierName { get; private set; }
+        public void EnterName(string carrierName)
+        {
+            Console.Write("Введiть назву корабля -> ");
+            CarrierName = carrierName;
+        }
+
+        public List<Ship> ships = new List<Ship>();
+
+        public Ship CreateShip()
+        {
+            Ship ship = new Ship();
+            ships.Add(ship);
+            ship.EnterName();
+            ship.EnterCapacity();
+            ship.EnterLoadCapacity();
+            ship.EnterFuelConsumption();
+            ship.EnterGraduationYear();
+            return ship;
+        }
+        public Ship CreateShip(string shipName, int capacity, int loadCapacity, int fuelConsumption, int graduationYear)
+        {
+            Ship ship = new Ship(shipName, capacity, loadCapacity, fuelConsumption, graduationYear);
+            ships.Add(ship);
+            return ship;
+
+        }
+
+
+        public void SortFuelConsumption()
+        {
+            ships.Sort(delegate (Ship ship1, Ship ship2)
+            { return ship1.FuelConsumption.CompareTo(ship2.FuelConsumption); });
+
+            for (int i = 0; i < ships.Count; i++)
+                Console.WriteLine(ships[i].ToString());
+        }
+        public int AllLoadCapacity()
+        {
+            int res = 0;
+            for (int i = 0; i < ships.Count; i++)
+                res += ships[i].LoadCapacity;
+            return res;
+        }
+        public int AllCapacity()
+        {
+            int res = 0;
+            for (int i = 0; i < ships.Count; i++)
+            {
+                res += ships[i].Capacity;
+            }
+            return res;
+        }
+        public void FindsShip()
+        {
+            Console.Write("Введiть мiнiмальний рiк: ");
+            int minYear = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введiть максимальний рiк: ");
+            int maxYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Результат пошуку: ");
+            bool isSearchSuccessful = false;
+            for (int i = 0; i < ships.Count; i++)
+            {
+                if ((minYear <= ships[i].GraduationYear) && (maxYear >= ships[i].GraduationYear))
+                {
+                    Console.WriteLine(ships[i].ToString());
+                    isSearchSuccessful = true;
+                }
+            }
+            if (!isSearchSuccessful)
+            {
+                Console.WriteLine("Корабля не знайдено!!!");
+            }
+        }
+
+        public Carrier(string carrierName)
+        {
+            CarrierName = carrierName;
+        }
+        public Carrier()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Iм'я: {CarrierName}, к-сть кораблiв: {ships.Count}");
+        }
+    }
+}
---
+++ lab2/Exercise_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Exercise_2
+{
+   
+    class Program
+    {
+        public static int CarrierChoise()
+        {
+            Console.WriteLine("Виберіть перевізника:" +
+                "\n1)Перевiзник 1 -> Tom" +
+                "\n2)Перевiзник 2 -> Bob");
+            int res = Convert.ToInt32(Console.ReadLine());
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            Port port = new Port();
+
+            Carrier carrierOne = port.CreateCarrier("Tom");
+            #region Створення кораблів
+            Ship ship11 = carrierOne.CreateShip("Ship11", 2000, 400, 100, 1995);
+            Ship ship12 = carrierOne.CreateShip("Ship12", 3000, 600, 140, 1999);
+            Ship ship13 = carrierOne.CreateShip("Ship13", 1000, 200, 60, 1987);
+            Ship ship14 = carrierOne.CreateShip("Ship14", 4000, 800, 170, 2008);
+            #endregion
+
+            Carrier carrierTwo = port.CreateCarrier("Bob");
+            #region Створення кораблів
+            Ship ship21 = carrierTwo.CreateShip("Ship21", 4000, 800, 200, 1998);
+            Ship ship22 = carrierTwo.CreateShip("Ship22", 6000, 1200, 280, 2002);
+            Ship ship23 = carrierTwo.CreateShip("Ship23", 2000, 400, 120, 1990);
+            Ship ship24 = carrierTwo.CreateShip("Ship24", 8000, 1600, 340, 2014);
+            #endregion
+
+
+            int numOfCarrier = CarrierChoise();
+            Console.WriteLine("Введiть бажану операцiю:" +
+                    "\n1) Вiдсортувати за витратами палива" +
+                    "\n2) Загальна вантажопiдйомнiсть" +
+                    "\n3) Загальна мiсткiсть" +
+                    "\n4) Пошук корабля за роком" +
+                    "\n5) Вихiд");
+            bool bl = true;
+            do
+            {
+                Console.WriteLine();
+                int operation = Convert.ToInt32(Console.ReadLine());
+                switch (operation)
+                {
+                    case 1: port.SortFuelConsumption(numOfCarrier); break;
+                    case 2: port.AllLoadCapacity(numOfCarrier); break;
+                    case 3: port.AllCapacity(numOfCarrier); break;
+                    case 4: port.FindsShip(numOfCarrier); break;
+                    case 5: bl = false; break;
+                    default:
+                        Console.WriteLine("Операція введена невiрно!!!");
+                        break;
+                }
+            }
+            while (bl);
+            Console.ReadKey();
+        }
+    }
+}
